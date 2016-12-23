73e6ace52a01912684c4794b99fa57369222ee2c
leehoy <leehoy12345@gmail.com>
2016-12-23 14:02:49 +0900

    Delete matlab files

--- ParallelBeamProjection.m
+++
@@ -1,7 +0,0 @@
-P=phantom('Modified Shepp-Logan',512);
-theta=linspace(0,360,180);
-[R,xp]=radon(P,theta);
-imshow(R,[]);
-% f=fopen('D:\workspace\Reconstruction\src\parallel.dat','w');
-% fwrite(f,R,'float32');
-% fclose(f);
