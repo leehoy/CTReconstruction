bfad5d08e62dfa9f5e15852367ca439bcd69703c
Hoyeon Lee <leehoy@kaist.ac.kr>
2017-02-19 11:06:29 +0900

    Forward projection function

---
+++ matlab/ForwardPorjection.m
@@ -0,0 +1,13 @@
+function [ output_args ] = ForwardProjection( image,parameters, type)
+%Forward projection function for iterative reconstruction
+%   image : reconstructed images to be forward projected
+%   parameters : parameters for the forward projection (dictionary)
+%   type : method to forward project images (string)
+%   type can be 'ray-driven', 'pixel-driven', 'distance-driven', and
+%   'separable-footprint'.
+
+
+
+
+end
+
