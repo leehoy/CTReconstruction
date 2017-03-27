6dab01c9088b8877157defc9d803b15096ef36e4
leehoy <leehoy12345@gmail.com>
2017-03-27 21:40:48 +0900

    source point defined

--- matlab/ForwardPorjection.m
+++ matlab/ForwardPorjection.m
@@ -6,15 +6,14 @@ function [ output ] = ForwardProjection( image,parameters)
 %   type can be 'ray-driven', 'pixel-driven', 'distance-driven', and
 %   'separable-footprint'
 type=parameters.type;
-DetectorOffset=parameters.CenterOfDetector;
-Source=parameters.CenterOfSource;
+DetectorOffset=parameters.DetectorCenterOffset;
+Source=parameters.InitialSource;
 SAD=parameters.SourceToAxis;
 SDD=parameters.SourceToDetector;
-DetectorCenter=
 numRows=parameters.NumberOfRows;
 numChan=parameters.NumberOfChannels;
-pixWidth=prameters.PixelWidth;
-pixHeight=parameters.PixelHeight;
+pixWidth=prameters.DetectorPixelSize[0];
+pixHeight=parameters.DetectorPixelSize[1];
 NumberOfViews=parameters.NumberOfViews;
 proj=zeros(numChan,numRow,NumberOfViews);
 Angle=linspace(0,AngleCoverage,NumberOfViews+1);% doesn't include last point of angle coverage
@@ -42,36 +41,43 @@ end
 
 end
 function distance(image,parameter,ViewAngle)
-VerticalCoordinate;
-HorizontalCooridnate;
-[CellCenterX,CellCenterY]=meshgrid(VerticalCoordinate,HorizontalCoordinate);
-CellBoundary1X=CellCenterX;CellBoudnary1Y=CellCenterY;
-CellBoundary2X=CellCenterX;
-CellBoundary2Y=CellCenterY;
-CellBoundary3
-[nx,ny,nz]=parameter.ObjectSize;
-Source=parameter.CenterOfSource;
-DetectorCenter;
-if(abs(Source(1)-DetectorCenter(1))>abs(Source(2)-DetectorCenter(2)))
-    nn=nx;
-    nDir=1;
-else
-    nn=ny;
-    nDir=2;
-end
-for i=1:nn
-    if nDir==1
-        k1=(Source(2)-)/(Source(1)-);
-        k2=(Source(2)-)/(Source(1)-);
-        Y1=floor(k1*(nx+i-0.5)+Source(2));
-        Y2=floor(k2*(nx+i-0.5)+Source(2));
-    else
-    end
-    if (Y1==Y2 && Z1==Z2)
-    elseif(Y1==Y2 && Z1~=Z2)
-    elseif(Y1~=Y2 && Z1==Z2)
-    else
-    end
+  Source=[parameter.SourceToAxis*sin(-ViewAngle),parameter.SourceToAxis*cos(-ViewAngle);
+  DetectorCenter=[parameters.SourceToDetector*sin(ViewAngle),parameter.SourceToDetector*cos(ViewAngle)]; % detector center is center of cntral bin
+  VerticalCoordinate;
+  HorizontalCooridnate;
+  [CellCenterH,CellCenterV]=meshgrid(VerticalCoordinate,HorizontalCoordinate);
+  CellBoundaryH1=CellCenterX;CellBoundaryH2=CellCenterX;
+  CellBoundaryV1=CellCenterY;CellBoundaryV2=CellCenterY;
+  CellBoundary3
+  [nx,ny,nz]=parameter.ObjectSize;
+  if(abs(Source(1)-DetectorCenter(1))>abs(Source(2)-DetectorCenter(2)))
+      nn=nx;
+      nDir=1;
+  else
+      nn=ny;
+      nDir=2;
+  end
+  for i=1:nn
+      if nDir==1
+          k1=(Source(1)-CellBoundaryH1)/(Source(2)-DetectorCenter(2));
+          k2=(Source(1)-CellBoundaryH2)/(Source(2)-DetectorCenter(2));
+          Y1=floor(k1*(nx+i-0.5)+Source(2));
+          Y2=floor(k2*(nx+i-0.5)+Source(2));
+          k1=(Source(1)-CellBoundaryV1)/(Source(2)-DetectorCenter(2));
+          k2=(Source(1)-CellBoundaryV2)/(Source(2)-DetectorCenter(2));
+          Y1=floor(k1*(nx+i-0.5)+Source(2));
+          Y2=floor(k2*(nx+i-0.5)+Source(2));
+      else
+          k1=(Source(1)-CellBoundaryH1)/(Source(2)-CellBoudaryV1);
+          k2=(Source(1)-CellBoundaryH2)/(Source(2)-CellBoundaryV2);
+          Y1=floor(k1*(nx+i-0.5)+Source(2));
+          Y2=floor(k2*(nx+i-0.5)+Source(2));
+      end
+      if (Y1==Y2 && Z1==Z2)
+      elseif(Y1==Y2 && Z1~=Z2)
+      elseif(Y1~=Y2 && Z1==Z2)
+      else
+      end
 end
 end
 function ray(image,parameter,ViewAngle)
