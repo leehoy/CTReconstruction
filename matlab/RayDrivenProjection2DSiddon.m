e03065365b679e9a3fb036cb0f3f5bbfc9bb24dc
user <user@DESKTOP-N72RB9V>
2017-07-12 10:01:40 +0900

    Unneccessary for-loop and variables removed

--- matlab/RayDrivenProjection2DSiddon.m
+++ matlab/RayDrivenProjection2DSiddon.m
@@ -3,6 +3,7 @@
 % three-dimensional CT array, Robert L. Siddon, Medical Physics, 12(2)
 % (1985).
 tic;
+%% Input parameters
 nx=512;
 ny=nx;
 ph=phantom(nx);
@@ -10,22 +11,30 @@ ph=phantom(nx);
 Source_init=[0,1000]; % Initial source position
 Detector_init=[0,-500]; % Initial detector position
 Origin=[0,0]; % Rotating center
-SAD=sqrt(sum((Source_init-Origin).^2));
-SDD=sqrt(sum((Source_init-Detector_init).^2));
 DetectorPixelSize=0.5; % Detector pixel spacing
 % DetectorPixelSize=445.059/750; % Detector pixel spacing
 NumberOfDetectorPixels=[1024 ,1]; % Number of detector rows and chnnels
 % NumberOfDetectorPixels=[750 ,1]; % Number of detector rows and chnnels
 PhantomCenter=[0,0]; % Center of phantom
-% dx=(2*SAD*sin(atan((445.059/2)/SDD)))/512; %phantom pixel spacing
-% dy=dx;
-dx=0.5; %phantom pixel spacing
-dy=-0.5;
+PhantomPixelSpacingX=0.5;
+PhantomPixelSpacingY=0.5;
 nTheta=360;
-% nTheta=680;
 StartAngle=0;
 EndAngle=2*pi;
 dir='ccw'; % direction may be counter-clockwise or clockwise
+
+
+%% Calculating parameters based on input parameters
+SAD=sqrt(sum((Source_init-Origin).^2));
+SDD=sqrt(sum((Source_init-Detector_init).^2));
+% dx=(2*SAD*sin(atan((445.059/2)/SDD)))/512; %phantom pixel spacing
+% dy=dx;
+dx=PhantomPixelSpacingX; %phantom pixel spacing
+dy=-PhantomPixelSpacingY;
+% Y index starts from top of the image which has the highest y coordinate
+% value, if it changes sign of dy and condition in line 96 should be
+% changed
+
 
 tol_min=1e-6;
 tol_max=1e6;
@@ -45,6 +54,8 @@ proj=zeros(NumberOfDetectorPixels(1),nTheta);
 %       bottom/right position from the center and it changes to up/left
 %       between 90 to 270 degrees -> Solved
 weight_map=zeros([size(ph,1),size(ph,2),nTheta]);
+
+%% Forward projection
 for angle_index=1:nTheta
     if(strcmp(dir,'ccw'))
     elseif(strcmp(dir,'cw'))
@@ -93,21 +104,16 @@ for angle_index=1:nTheta
             j_min=ceil((ny+1)-(Yplane(end)-alpha_min*(DetectorIndex(2,detector_index)-SourceY)-SourceY)/dy);
             j_max=floor(1+(SourceY+alpha_max*(DetectorIndex(2,detector_index)-SourceY)-Yplane(1))/dy);
             alpha_y=alpha_y(j_min:j_max);
-%             alpha_y=alpha_y(j_min:-1:j_max);
         else
             j_min=ceil((ny+1)-(Yplane(end)-alpha_max*(DetectorIndex(2,detector_index)-SourceY)-SourceY)/dy);
             j_max=floor(1+(SourceY+alpha_min*(DetectorIndex(2,detector_index)-SourceY)-Yplane(1))/dy);
             alpha_y=alpha_y(j_max:-1:j_min);
-%             alpha_y=alpha_y(j_max:j_min);
         end
         alpha=uniquetol(sort([alpha_min,alpha_x,alpha_y,alpha_max]),tol_min/alpha_max);
         l=zeros(length(alpha)-1,1);
         d12=sqrt((SourceX-DetectorIndex(1,detector_index))^2+(SourceY-DetectorIndex(2,detector_index))^2);
         for i=1:length(l)
             l(i)=d12*(alpha(i+1)-alpha(i));
-        end
-        index=zeros(length(l),2);
-        for i=1:size(index,1)
             alpha_mid=(alpha(i+1)+alpha(i))/2;
             xx=(SourceX+alpha_mid*(DetectorIndex(1,detector_index)-SourceX)-Xplane(1))/dx;
             yy=(SourceY+alpha_mid*(DetectorIndex(2,detector_index)-SourceY)-Yplane(1))/dy;
@@ -117,16 +123,11 @@ for angle_index=1:nTheta
             if(abs(yy)<=tol_min)
                 yy=0;
             end
-            index(i,2)=floor(xx+1);
-            index(i,1)=floor(yy+1);
+            index_x=floor(xx+1);
+            index_y=floor(yy+1);
+            proj(detector_index,angle_index)=proj(detector_index,angle_index)+l(i)*ph(index_y,index_x);
+            weight_map(index_y,index_x,angle_index)=weight_map(index_y,index_x,angle_index)+l(i);
         end
-        for i=1:length(l)
-            proj(detector_index,angle_index)=proj(detector_index,angle_index)+l(i)*ph(index(i,1),index(i,2));
-            weight_map(index(i,1),index(i,2),angle_index)=weight_map(index(i,1),index(i,2),angle_index)+l(i);
-        end
-%         plot(l);
     end
 end
-% imagesc(proj);
-% colormap gray;
 toc
