785834e40f8cefa565eeea1b5fa4756a0b38e625
user <user@DESKTOP-N72RB9V>
2017-09-01 18:33:09 +0900

    Distance-driven backprojection weight using Gao's method to compute contributing pixels

---
+++ matlab/DistanceDrivenBackprojection2D.m
@@ -0,0 +1,114 @@
+function [ recon ] = DistanceDrivenBackprojection2D( proj )
+%UNTITLED6 이 함수의 요약 설명 위치
+%   자세한 설명 위치
+    N=size(proj,1);
+    M=size(proj,2);
+%     DetectorWidth=N;
+    R=1000;
+    D=500;
+    r_spacing=0.5;
+    deltaS=r_spacing*R/(R+D);
+    nx=256;
+    ny=nx;
+%     DetectorSize=r_spacing*DetectorWidth;
+    gamma=((0:N-1)-(N-1)/2)*deltaS;
+    ZeroPaddedLength=2^nextpow2(2*(N-1));
+    cutoff=0.3;
+    FilterType='ram-lak';
+    filter=FilterLine(ZeroPaddedLength+1,deltaS,FilterType,cutoff)*0.5;
+%     fov=2*R*sin(atan((DetectorSize/2)/(R+D)));
+    ReconSpacingX=0.5; % fov/nx;
+    ReconSpacingY=-0.5;
+    recon_planeX=(-nx/2+(0:nx))*ReconSpacingX; % pixel boundaries of image
+    recon_planeY=(-ny/2+(0:ny))*ReconSpacingY;
+    recon_planeX=recon_planeX-ReconSpacingX/2;
+    recon_planeY=recon_planeY-ReconSpacingY/2;
+%     x=(-(nx-1)/2:(nx-1)/2)*ReconSpacingX;
+%     y=(-(ny-1)/2:(ny-1)/2)*ReconSpacingY;
+%     [X,Y]=meshgrid(x,y);
+%     xpr=X;
+%     ypr=Y;
+    recon=zeros(nx,ny);
+%     [phi,r]=cart2pol(xpr,ypr);
+    theta=linspace(0,360,M+1);
+    theta=theta*(pi/180);
+    dtheta=(pi*2)/M;
+    for i=1:M
+        R1=proj(:,i);
+        w=((R)./sqrt((R)^2+gamma'.^2));
+        angle=theta(i);
+        R2=w.*R1;
+        Q=real(ifft(ifftshift(fftshift(fft(R2,ZeroPaddedLength)).*filter)));
+        Q=Q(1:length(R2));
+%         fprintf('%d\n',i);
+%         [DetectorBoundary1,DetectorBoundary2]=DetectorBoundaryFinder(N,r_spacing,angle,D);
+        recon=recon+backproj(Q,recon_planeX,recon_planeY,angle,R,R+D,N)*dtheta;
+    end
+    close all;
+    imshow(recon,[]);
+end
+function bp=backproj(proj,recon_planeX,recon_planeY,angle,SAD,SDD,nd)
+    nx=length(recon_planeX)-1;
+    ny=length(recon_planeY)-1;
+    bp=zeros(nx,ny);
+    dx=recon_planeX(2)-recon_planeX(1);
+    dy=recon_planeY(2)-recon_planeY(1);
+    recon_PixelsX=recon_planeX(1:end-1)+dx/2; %x center of the pixels
+    recon_PixelsY=recon_planeY(1:end-1)+dy/2; %y center of the pixels
+    fprintf('%f\n',angle);
+%     recon_PixelsX_new=recon_PixelsX*cos(angle)+recon_PixelsY*sin(angle);
+%     recon_PixelsY_new=-recon_PixelsX*sin(angle)+recon_PixelsY*cos(angle);
+    
+%     recon_PixelsX_new=recon_PixelsX*cos(angle)-recon_PixelsY*sin(angle);
+%     recon_PixelsY_new=recon_PixelsX*sin(angle)+recon_PixelsY*cos(angle);
+%     assert(min(n_min(:))>0 && max(n_min(:))<nd+1 && min(n_min(:))>0 && max(n_max(:))<nd+1);
+    for i=1:nx
+        for j=1:ny
+            xr=recon_PixelsX(i)*cos(angle)+recon_PixelsY(j)*sin(angle);
+            yr=-recon_PixelsX(i)*sin(angle)+recon_PixelsY(j)*cos(angle);
+            n_min=((yr-0.25)/(xr+SAD))*SDD/0.5+nd/2;
+            n_max=((yr+0.25)/(xr+SAD))*SDD/0.5+nd/2;
+%             beam path length need to be considered
+            for k=floor(n_min):floor(n_max)
+                if(k<1 || k>nx)
+                    continue;
+                end
+                if(k==floor(n_min))
+                    bp(i,j)=proj(k)*(ceil(n_min)-n_min)/(n_max-n_min);
+                elseif(k>floor(n_min) && k<floor(n_max))
+                    bp(i,j)=proj(k)*(n_max-n_min);
+                elseif(k==floor(n_max))
+                    bp(i,j)=proj(k)*(n_max-floor(n_max))/(n_max-n_min);
+                else
+                    fprintf('????\n');
+                end
+            end
+        end
+    end
+end
+function [DetectorBoundary1,DetectorBoundary2]=DetectorBoundaryFinder(NumberOfDetectorPixels,...
+    DetectorPixelSize,angle,Distance)
+    DetectorX=Distance*sin(angle);  % center of detector coordinate
+    DetectorY=-Distance*cos(angle);
+    DetectorLength=(floor(-NumberOfDetectorPixels(1)/2):floor(NumberOfDetectorPixels(1)/2))*DetectorPixelSize;
+    if(abs(tan(theta(angle_index)))<=1e-6) % detector is parallel to x-axis
+        DetectorIndex=[DetectorX+DetectorLength; repmat(DetectorY,1,size(DetectorLength,2))];
+    elseif(tan(theta(angle_index))>=1e6) % detector is parallel to y-axis
+        DetectorIndex=[repmat(DetectorX,1,size(DetectorLength,2)); DetectorY+DetectorLength];
+    else
+        xx=sqrt(DetectorLength.^2./(1+tan(theta(angle_index))^2));
+        yy=tan(theta(angle_index))*sqrt(DetectorLength.^2./(1+tan(theta(angle_index))^2));
+        DetectorIndex=[DetectorX+sign(DetectorLength).*xx;...
+            DetectorY+sign(DetectorLength).*yy];
+    end
+    if(DetectorY>0)
+        DetectorIndex=DetectorIndex(:,end:-1:1);
+    end
+    DetectorIndex=DetectorIndex(:,1:end-1); % The index pointing center of detector pixels
+    DetectorBoundary1=[DetectorIndex(1,detector_index)-cos(theta(angle_index))*...
+                DetectorPixelSize/2,DetectorIndex(2,detector_index)-sin(theta(angle_index))*...
+                DetectorPixelSize/2];
+    DetectorBoundary2=[DetectorIndex(1,detector_index)+cos(theta(angle_index))*...
+        DetectorPixelSize/2,DetectorIndex(2,detector_index)+sin(theta(angle_index))*...
+        DetectorPixelSize/2];
+end
